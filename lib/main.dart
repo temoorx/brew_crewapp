7822f3846416931d31c4db3b1e848e571fe9b09f
FlutLab.io <>
2022-05-13 11:16:18 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:brew_crew/screens/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+void main() async => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
